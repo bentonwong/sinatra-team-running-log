2b049ebab3ea361d97e23c055fa654546ba033fe
bentonwong <bentonwong@gmail.com>
2017-03-27 12:01:35 -0700

    add migration files

--- db/migrate/20170327175401_create_runners.rb
+++ db/migrate/20170327175401_create_runners.rb
@@ -1,4 +1,11 @@
-class CreateRunners < ActiveRecord::Migration[5.0]
+class CreateRunners < ActiveRecord::Migration
   def change
+    create_table :runners do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :name
+      t.date :birthday
+      t.string :gender
+    end
   end
 end
