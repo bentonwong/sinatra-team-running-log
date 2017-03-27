359e04951fe3069adc4d9c57274f13b823f27c0c
bentonwong <bentonwong@gmail.com>
2017-03-27 12:09:39 -0700

    stub out workout and runner models

--- db/migrate/20170327175412_create_workouts.rb
+++ db/migrate/20170327175412_create_workouts.rb
@@ -1,4 +1,11 @@
-class CreateWorkouts < ActiveRecord::Migration[5.0]
+class CreateWorkouts < ActiveRecord::Migration
   def change
+    create_table :workouts do |t|
+      t.date :workout_date
+      t.decimal :distance
+      t.integer  :runner_id
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
   end
 end
