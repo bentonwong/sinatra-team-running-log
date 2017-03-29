333f927cd5bab56457813c5ff2af2891a55196bd
bentonwong <bentonwong@gmail.com>
2017-03-29 11:33:24 -0700

    refactor rankings method

--- app/controllers/concerns/helpers.rb
+++ app/controllers/concerns/helpers.rb
@@ -12,18 +12,23 @@ module Helpers
 
   def rankings(gender,period)
     total_mileage_by_runner_id = Hash.new(0)
+
     case period
       when "All Time"
-        query_results = Workout.where("gender = ?", gender).joins("INNER JOIN runners ON runners.id = workouts.runner_id")
+        start_date = 0
       when "This Year"
-        query_results = Workout.where("gender = ? AND workout_date >= ?", gender, Time.now.beginning_of_year).joins("INNER JOIN runners ON runners.id = workouts.runner_id")
+        start_date = Time.now.beginning_of_year
       when "This Month"
-        query_results = Workout.where("gender = ? AND workout_date >?", gender, Time.now.beginning_of_month).joins("INNER JOIN runners ON runners.id = workouts.runner_id")
+        start_date = Time.now.beginning_of_month
     end
-    query_results.each do |workout|
+
+    query_results = Workout.where("gender = ? AND workout_date >= ?", gender, start_date).joins("INNER JOIN runners ON runners.id = workouts.runner_id") #returns the requested workouts matching conditions
+
+    query_results.each do |workout| #sums up the mileage by runner_id and puts it into a hash
       total_mileage_by_runner_id[workout.runner_id] += workout.distance
     end
-    total_mileage_by_runner_id.sort_by{|runner_id, mileage| mileage}.reverse
+
+    total_mileage_by_runner_id.sort_by{|runner_id, mileage| mileage}.reverse #sorts the hash by mileage and puts it into an array
   end
 
 end
