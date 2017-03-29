ce3f181dc10f0a3185be93d65cb010e3373d1e0c
bentonwong <bentonwong@gmail.com>
2017-03-29 11:10:22 -0700

    fix bugs with leaderboard.erb and rankings method

--- app/controllers/concerns/helpers.rb
+++ app/controllers/concerns/helpers.rb
@@ -11,33 +11,13 @@ module Helpers
   end
 
   def rankings(gender,period)
-    total_mileage = {}
-
-    case period
-      when "this year"
-        Runner.all.select{|runner| runner[:gender] == gender}.each do |runner|
-          total_mileage[runner.id] = Workout.all.select{|w| w.runner_id == runner.id && Time.now.beginning_of_year <= w.workout_date}.map{|workout| workout.distance}.sum
-        end
-      when "this month"
-        Runner.all.select{|runner| runner[:gender] == gender}.each do |runner|
-          total_mileage[runner.id] = Workout.all.select{|w| w.runner_id == runner.id && Time.now.beginning_of_month <= w.workout_date}.map{|workout| workout.distance}.sum
-        end
-      when "all time"
-        Runner.all.select{|runner| runner[:gender] == gender}.each do |runner|
-          total_mileage[runner.id] = Workout.all.select{|w| w.runner_id == runner.id}.map{|workout| workout.distance}.sum
-        end
-    end
-    total_mileage.sort_by{|runner_id, mileage| mileage}.reverse
-  end
-
-  def rankings2(gender,period)
     total_mileage_by_runner_id = Hash.new(0)
     case period
-      when "all time"
+      when "All Time"
         query_results = Workout.where("gender = ?", gender).joins("INNER JOIN runners ON runners.id = workouts.runner_id")
-      when "this year"
+      when "This Year"
         query_results = Workout.where("gender = ? AND workout_date >= ?", gender, Time.now.beginning_of_year).joins("INNER JOIN runners ON runners.id = workouts.runner_id")
-      when "this month"
+      when "This Month"
         query_results = Workout.where("gender = ? AND workout_date >?", gender, Time.now.beginning_of_month).joins("INNER JOIN runners ON runners.id = workouts.runner_id")
     end
     query_results.each do |workout|
