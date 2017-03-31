9ffb3d3d40fefc417bf75873348d4b32ac2ff35e
bentonwong <bentonwong@gmail.com>
2017-03-30 23:26:27 -0700

    move rankings method from helpers to Workout model

--- app/controllers/concerns/helpers.rb
+++ app/controllers/concerns/helpers.rb
@@ -19,23 +19,7 @@ module Helpers
   end
 
   def rankings(gender,period)
-    case period
-      when "All Time"
-        start_date = 0
-      when "This Year"
-        start_date = Time.now.beginning_of_year
-      when "This Month"
-        start_date = Time.now.beginning_of_month
-    end
-    query_results = Workout.where("gender = ? AND workout_date >= ?", gender, start_date).joins("INNER JOIN runners ON runners.id = workouts.runner_id")
-    format_query_results_into_rankings(query_results)
-  end
-
-  def format_query_results_into_rankings(query_results)
-    total_mileage_by_runner_id = Hash.new(0)
-    query_results.each{|workout| total_mileage_by_runner_id[workout.runner_id] += workout.distance}
-    total_mileage_by_runner = total_mileage_by_runner_id.sort_by{|runner_id, mileage| mileage}.reverse
-    total_mileage_by_runner.collect{|runner_data| runner_data.insert(1,find_runner_by_id(runner_data[0]).name)}
+    Workout.rankings(gender,period)
   end
 
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,24 @@ class Workout < ActiveRecord::Base
 
   belongs_to :runner
 
+  def self.rankings(gender,period)
+    case period
+      when "All Time"
+        start_date = 0
+      when "This Year"
+        start_date = Time.now.beginning_of_year
+      when "This Month"
+        start_date = Time.now.beginning_of_month
+    end
+    query_results = self.where("gender = ? AND workout_date >= ?", gender, start_date).joins("INNER JOIN runners ON runners.id = workouts.runner_id")
+    self.format_query_results_into_rankings(query_results)
+  end
+
+  def self.format_query_results_into_rankings(query_results)
+    total_mileage_by_runner_id = Hash.new(0)
+    query_results.each{|workout| total_mileage_by_runner_id[workout.runner_id] += workout.distance}
+    total_mileage_by_runner = total_mileage_by_runner_id.sort_by{|runner_id, mileage| mileage}.reverse
+    total_mileage_by_runner.collect{|runner_data| runner_data.insert(1,Runner.find_by_id(runner_data[0]).name)}
+  end
+
 end
