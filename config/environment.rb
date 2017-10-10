8daaf9ec8d36664e83ebe0013ab809e7f9580964
bentonwong <bentonwong@gmail.com>
2017-10-09 23:38:13 -0700

    update database url

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
 ENV['SINATRA_ENV'] ||= "development"
+ENV['DATABASE_URL'] = kgydaitjdbxgrk:349710f64d899a88bc92f2ca0ef2c7d5a9a1cb8baa60388accab731a81fe2cbf@ec2-184-72-245-58.compute-1.
+amazonaws.com:5432/ddcd6tfcn01d8r
+
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
